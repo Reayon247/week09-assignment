37bb00a41507029c7fbfde53dd8bdcf7dfda34b7
Ryan Nugent <ryan.nugent247@gmail.com>
2025-06-28 20:25:51 +0100

    feat: added a dropdown menu from component library

--- src/Components/Posts.tsx
+++ src/Components/Posts.tsx
@@ -2,6 +2,7 @@ import { postType } from "@/types/dataTypes";
 import { db } from "@/utils/dbConnection";
 import Link from "next/link";
 import styles from "@/styles/Posts.module.css";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
 export default async function Posts() {
   const query = await db.query("SELECT * FROM week9post");
@@ -14,7 +15,24 @@ export default async function Posts() {
       {posts.map((post: postType) => {
         return (
           <div key={post.id} className={styles.postcon}>
-            <Link href={`/user/${post.username}`}>{post.username}:</Link>
+            <div className={styles.nameanddrop}>
+              <Link href={`/user/${post.username}`}>{post.username}:</Link>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger className={styles.trigger}>
+                  Settings
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Portal>
+                  <DropdownMenu.Content className={styles.Content}>
+                    <DropdownMenu.Item className={styles.Item}>
+                      Edit
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item className={styles.Item}>
+                      Delete
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Portal>
+              </DropdownMenu.Root>
+            </div>
             <p>{post.post}</p>
             <p>Dislikes: {post.dislikes}</p>
             <p>{post.date}</p>
